e82042811a5d082c23017ff8cd9c307c18890d30
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-09-30 21:38:07 +0530

    opts: basic options

---
+++ lua/config/options.lua
@@ -0,0 +1,13 @@
+local opt = vim.opt
+local o = vim.o
+local g = vim.g
+
+g.mapleader = " "
+
+o.number = true
+o.relativenumber = true
+o.numberwidth = 2
+o.ruler = false
+
+-- Disable the OG nvim intro
+opt.shortmess:append "sI"
