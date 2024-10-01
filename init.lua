a7b8c18a8a47e935c51b4f38e700aee3258c6ae9
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-10-01 16:11:50 +0530

    refactor

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@ require("config.lazy")
 require("config.options")
 require("config.keymaps")
 
-require("plugins.telescope")
+require("plugins.barbar")
 require("plugins.nvim-tree")
+require("plugins.telescope")
