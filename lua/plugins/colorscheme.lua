74501b24e6f972e29c196bfe18d0108b4ee4fd99
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-10-05 13:10:46 +0530

    change from tj to mine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,37 +1,37 @@
 return {{ -- Lua
-   "powrhouseofthecell/colorbuddy.nvim",
-   config = function()
-      vim.cmd.colorscheme("gruvbuddy")
-   end
+    -- Add to the package manager?
+    "powrhouseofthecell/colorbuddy.nvim",
+    config = function()
+        vim.cmd.colorscheme("gruvbuddy")
+    end
 
-}
-   -- {
-   --     'RRethy/base16-nvim',
-   --     config = function()
-   --         require('base16-colorscheme').with_config({
-   --             telescope = true,
-   --             indentblankline = true,
-   --             notify = true,
-   --             ts_rainbow = true,
-   --             cmp = true,
-   --             illuminate = true,
-   --             dapui = true
-   --         })
-   --     end
-   -- } 
-   --    {
-   --     'rose-pine/neovim',
-   --     name = 'rose-pine',
-   --     config = function()
-   --         require('rose-pine').setup({
-   --             styles = {
-   --                 bold = true,
-   --                 italic = false,
-   --                 transparency = true
-   --             }
-   --         })
-   --         vim.cmd('colorscheme rose-pine')
-   --     end
-   -- }
+} -- {
+--     'RRethy/base16-nvim',
+--     config = function()
+--         require('base16-colorscheme').with_config({
+--             telescope = true,
+--             indentblankline = true,
+--             notify = true,
+--             ts_rainbow = true,
+--             cmp = true,
+--             illuminate = true,
+--             dapui = true
+--         })
+--     end
+-- } 
+--    {
+--     'rose-pine/neovim',
+--     name = 'rose-pine',
+--     config = function()
+--         require('rose-pine').setup({
+--             styles = {
+--                 bold = true,
+--                 italic = false,
+--                 transparency = true
+--             }
+--         })
+--         vim.cmd('colorscheme rose-pine')
+--     end
+-- }
 }
 
