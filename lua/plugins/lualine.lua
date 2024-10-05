82f8c7421889eda6590935dab7755607aa7cbe34
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-10-05 13:59:00 +0530

    plugin: change theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
         require('lualine').setup({
             options = {
                 icons_enabled = true,
-                theme = 'ayu_dark',
+                theme = 'base16',
                 component_separators = {
                     left = '',
                     right = ''
