977727f382fd2e127d10a15bcda29f7fcc14f2e4
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-10-10 14:50:31 +0530

    config: unused config telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,49 +3,6 @@ return {
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		require("telescope").setup({
-			defaults = {
-				-- configure to use ripgrep
-				vimgrep_arguments = {
-					"rg",
-					"--follow", -- Follow symbolic links
-					"--hidden", -- Search for hidden files
-					"--no-heading", -- Don't group matches by each file
-					"--with-filename", -- Print the file path with the matched lines
-					"--line-number", -- Show line numbers
-					"--column", -- Show column numbers
-					"--smart-case", -- Smart case search
-
-					-- Exclude some patterns from search
-					"--glob=!**/.git/*",
-					"--glob=!**/.idea/*",
-					"--glob=!**/.vscode/*",
-					"--glob=!**/build/*",
-					"--glob=!**/dist/*",
-					"--glob=!**/yarn.lock",
-					"--glob=!**/package-lock.json",
-				},
-			},
-			pickers = {
-				find_files = {
-					hidden = true,
-					-- no_ignore = true,
-					-- needed to exclude some files & dirs from general search
-					-- when not included or specified in .gitignore
-					find_command = {
-						"rg",
-						"--files",
-						"--hidden",
-						"--glob=!**/.git/*",
-						"--glob=!**/.idea/*",
-						"--glob=!**/.vscode/*",
-						"--glob=!**/build/*",
-						"--glob=!**/dist/*",
-						"--glob=!**/yarn.lock",
-						"--glob=!**/package-lock.json",
-					},
-				},
-			},
-		})
+		-- require("telescope").setup({})
 	end,
 }
