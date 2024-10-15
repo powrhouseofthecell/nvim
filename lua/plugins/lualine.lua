e0dbb8f58c2ff095277044be4d7b21d752dee053
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-10-15 15:05:21 +0530

    plugin: lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "ayu_mirage",
+				theme = "horizon",
 				component_separators = {
 					left = "",
 					right = "",
