8741b9286fc1e457111baf4fc6ab8f7394349764
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-10-21 16:07:22 +0530

    opts: tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,9 +12,9 @@ o.relativenumber = true
 o.numberwidth = 4
 o.ruler = false
 
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
 opt.expandtab = true
 
 opt.wrap = false
