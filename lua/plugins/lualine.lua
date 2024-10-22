2ee48f1304316ce2cbfe0a80457e4e475bed79c7
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-10-22 15:30:52 +0530

    theme: lualine theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "horizon",
+				theme = "iceberg_dark",
 				component_separators = {
 					left = "",
 					right = "",
