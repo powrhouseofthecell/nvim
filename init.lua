8ce8a46e7360a870c44080ffa09b7ce0c7905838
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-10-22 19:23:47 +0530

    func: LineNumberColors

--- init.lua
+++ init.lua
@@ -60,3 +60,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_user_command("ClearRegisters", clear_registers, {
 	desc = "Clear all registers",
 })
+
+-- Sets colors to line numbers Above, Current and Below  in this order
+function LineNumberColors()
+	-- vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#51B3EC", bold = true })
+	vim.api.nvim_set_hl(0, "LineNr", { fg = "lightgray", bold = false })
+	-- vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#FB508F", bold = true })
+end
+LineNumberColors()
