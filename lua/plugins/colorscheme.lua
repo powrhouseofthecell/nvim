6dedd0252b9836012c9a34db5893b2869f5cb14e
powrhouseofthecell <theshahzuhaib96@gmail.com>
2025-01-14 20:21:43 +0530

    colorscheme: kanagawa

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,17 @@
 return {
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			require("kanagawa").setup({
+				styles = {
+					bold = false,
+					italic = false,
+					transparency = true,
+				},
+			})
+			vim.cmd("colorscheme kanagawa")
+		end,
+	},
 	-- {
 	-- 	"powrhouseofthecell/colorbuddy.nvim",
 	-- 	config = function()
@@ -21,18 +34,18 @@ return {
 	--     end
 	-- }
 
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				styles = {
-					bold = true,
-					italic = false,
-					transparency = true,
-				},
-			})
-			vim.cmd("colorscheme rose-pine")
-		end,
-	},
+	-- {
+	-- 	"rose-pine/neovim",
+	-- 	name = "rose-pine",
+	-- 	config = function()
+	-- 		require("rose-pine").setup({
+	-- 			styles = {
+	-- 				bold = true,
+	-- 				italic = false,
+	-- 				transparency = true,
+	-- 			},
+	-- 		})
+	-- 		vim.cmd("colorscheme rose-pine")
+	-- 	end,
+	-- },
 }
