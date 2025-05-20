dbc0358befcca90a59a3c62d933dcb7182b86892
powrhouseofthecell <theshahzuhaib96@gmail.com>
2025-05-20 11:50:05 +0530

    chore: disable lua rocks

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,10 +32,13 @@ require("lazy").setup({
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
 	install = {
-		colorscheme = { "habamax" },
+		colorscheme = { "rose-pine" },
 	},
 	-- automatically check for plugin updates
 	checker = {
 		enabled = true,
+	},
+	rocks = {
+		enabled = false,
 	},
 })
